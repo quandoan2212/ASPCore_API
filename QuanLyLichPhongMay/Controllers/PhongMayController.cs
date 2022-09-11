a8aa8e41ada67beffe7d27af6d0fc71365e7922c
quandoan2212 <1851010108quan@ou.edu.vn>
2022-09-11 20:02:31 +0700

    update init

---
+++ QuanLyLichPhongMay/Controllers/PhongMayController.cs
@@ -0,0 +1,31 @@
+﻿using BLL;
+using Common.Req;
+using Common.Resp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuanLyLichPhongMay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhongMayController : ControllerBase
+    {
+        private PhongMaySrv phongMaySrv;
+        public PhongMayController()
+        {
+            this.phongMaySrv = new PhongMaySrv();
+        }
+
+        [HttpPost("get-by-id")]
+        public IActionResult GetPhongMayById([FromBody] SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res = phongMaySrv.Read(req.Keyword);
+            return Ok(res);
+        }
+    }
+}
